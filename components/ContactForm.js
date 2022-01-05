49fe1e3bea6d36ce614dc53f89a5c24e45519022
Francesco Ippolito <ipp.francesco@gmail.com>
2022-01-05 11:25:58 +0100

    contact form in palce, need styling

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -9,7 +9,7 @@ export default function ContactForm() {
             <div className="contact-box">
                 <form action="https://formsubmit.co/ipp.francesco@gmail.com" method="POST">
                     <input type="hidden" name="_subject" value="New submission!"></input>
-                    <textarea classNamelass="field" name="testo" placeholder="Cosa vuoi chiedere?" rows="4" cols="50" required></textarea>
+                    <textarea className="field" name="testo" placeholder="Cosa vuoi chiedere?" rows="4" cols="50" required></textarea>
                     <input className="field" type="text" name="name" required />
                     <input className="field" type="email" name="email" placeholder="Email Address" required></input>
                     <button className="btn" type="submit">Send</button>
--- pages/contact.js
+++ pages/contact.js
@@ -5,25 +5,15 @@ export default function Contact() {
 
 
     return (
-        <div className="not-found">
-            <h1>Contact Page</h1>
-            <h2>Form under construction</h2>
-            <div>
-                <ContactForm />
-            </div>
-            <p>Back to <Link href="/"> Homepage</Link> :D</p>
-
-
-            <style jsx>{`
-        .not-found {
-          background: #fff;
-          padding: 30px;
-          box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
-        }
-        h1 {
-          font-size: 3em;
-        }
-      `}</style>
+        <div className='container'>
+            <form action="https://formsubmit.co/ipp.francesco@gmail.com" method="POST">
+                <input type="hidden" name="_subject" value="New submission!" />
+                <input type="text" name="name" required placeholder='Nome' />
+                <input type="email" name="email" required placeholder='E-mail' />
+                <textarea type="text" name="testo" required placeholder='Cosa vuoi chiederci?' cols={15} rows={6}></textarea>
+                <button type="submit">Send</button>
+            </form>
         </div>
     )
+
 }
