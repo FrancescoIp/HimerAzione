d5fc28c5ff99196708cdfa4c3a207039d61bc498
Francesco Ippolito <ipp.francesco@gmail.com>
2022-01-05 11:36:12 +0100

    cleaning code in contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -6,13 +6,7 @@ export default function Contact() {
 
     return (
         <div className='container'>
-            <form action="https://formsubmit.co/ipp.francesco@gmail.com" method="POST">
-                <input type="hidden" name="_subject" value="New submission!" />
-                <input type="text" name="name" required placeholder='Nome' />
-                <input type="email" name="email" required placeholder='E-mail' />
-                <textarea type="text" name="testo" required placeholder='Cosa vuoi chiederci?' cols={15} rows={6}></textarea>
-                <button type="submit">Send</button>
-            </form>
+            <ContactForm/>
         </div>
     )
 
