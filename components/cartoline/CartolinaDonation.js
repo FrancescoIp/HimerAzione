d7cf195b993c4e31fcb9c277d29212de8542a706
Francesco Ippolito <ipp.francesco@gmail.com>
2022-01-18 11:20:59 +0100

    ultimate responsive design for donation cartoline

--- components/cartoline/CartolinaDonation.js
+++ components/cartoline/CartolinaDonation.js
@@ -4,7 +4,7 @@ export default function CartolinaDonation() {
 
     return (
         <div className='donation-body'>
-            <div>
+            <div className="content-container">
                 <h2>Donate tutti i vostri soldini per una causa magnifica :D</h2>
                 <button  className="donation-button"><a href="https://www.google.it">Dona qui</a></button>
             </div> 
