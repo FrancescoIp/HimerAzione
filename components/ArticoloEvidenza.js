208875112768e20e19c28a61997236b81a678b03
Francesco Ippolito <ipp.francesco@gmail.com>
2022-02-22 18:26:21 +0100

    evidenza done

--- components/ArticoloEvidenza.js
+++ components/ArticoloEvidenza.js
@@ -1,20 +1,43 @@
-import ArticoloCard from "./ArticoloCard"
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import dayjs from 'dayjs'
+import { Col, Row } from 'react-bootstrap'
 
-export default function ArticoloEvidenza({articoli}) {
-
-    const articoliEvidenza = articoli.filter((articolo)=>{
-        
+export default function ArticoloEvidenza({ articoli }) {
+    const articoliEvidenza = articoli.filter((articolo) => {
         return articolo.fields.evidenza
     })
 
-    return(
-        <div>
-            {articoliEvidenza.map((articoloE)=>{
-                const { title, slug, immagineCopertina, publishedDate, tags } = articoloE.fields
-                return(
-                    <p key={title}>{title}</p>
+    return (
+        <div className="evidenza-container">
+            {articoliEvidenza.map((articoliE) => {
+                const { title, slug, immagineCopertina, publishedDate, tags } = articoliE.fields
+                return (
+                    <Col xs={12} className='card-container-evidenza mb-5'>
+                        <Link href={'/blog/' + slug}>
+                            <a>
+                                <Row>
+                                    <Col className='image-card-container p-6' xs={5} >
+                                        <Image
+                                            src={'https:' + immagineCopertina.fields.file.url}
+                                            width={640}
+                                            height={200}
+                                            layout='responsive'
+                                            id='BlogPrevCardImg'
+                                        />
+                                    </Col>
+                                    
+                                    <Col className='text-container-evidenza' >
+                                        <h3>{title}</h3>
+                                    </Col>
+                                </Row>
+                            </a>
+                        </Link>
+                    </Col>
                 )
             })}
+
         </div>
     )
 }
