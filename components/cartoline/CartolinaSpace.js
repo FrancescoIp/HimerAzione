f3986e7290df6955839e82461bb9728a15d92220
Francesco Ippolito <ipp.francesco@gmail.com>
2022-02-28 11:06:53 +0100

    style fix

--- components/cartoline/CartolinaSpace.js
+++ components/cartoline/CartolinaSpace.js
@@ -5,21 +5,16 @@ export default function CartolinaSPace() {
     return (
         <div className='cartolina-space-container'>
             <Row>
-                <Col xs={7} className='align-self-center'>
+                <Col xs={12} className='align-self-center'>
                     <Image
-                        src='/space1.png'
+                        src='/himerazionefotodigruppo.jpeg'
                         width={608}
                         height={456}
                         layout='responsive'
                     />
                 </Col>
-                <Col xs={5}>
-                    <p>HimerAzione, verso l'infinito e oltre!!</p>
-                    <span className='source-link'>
-                        Illustration by
-                        <a href="https://icons8.com/illustrations/author/5c07e68d82bcbc0092519bb6">
-                            Icons 8</a> from <a href="https://icons8.com/illustrations">Ouch!</a>
-                    </span>
+                <Col xs={12} >
+                    <p>Ecco la banda, ma ne manca qualcuno!</p>
                 </Col>
             </Row>
         </div>
