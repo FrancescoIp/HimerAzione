0a105a8e8e22100818b5c957878b09589ff40209
Francesco Ippolito <ipp.francesco@gmail.com>
2022-05-11 10:53:10 +0200

    add more translation

--- components/Header.js
+++ components/Header.js
@@ -1,115 +1,139 @@
-import { Row, Col, Navbar, NavDropdown } from 'react-bootstrap'
+import { Row, Col, Navbar, NavDropdown } from "react-bootstrap";
 import { motion } from "framer-motion";
-import Link from 'next/link'
-import Image from 'next/image'
-import NavLink from './NavLink';
-import { useRouter } from 'next/router'
-import useTranslation from 'next-translate/useTranslation'
+import Link from "next/link";
+import Image from "next/image";
+import NavLink from "./NavLink";
+import { useRouter } from "next/router";
+import useTranslation from "next-translate/useTranslation";
 
 export default function Header({ pageMarginTop, setPageMarginTop }) {
-    let { t } = useTranslation()
-    let router = useRouter()
+  let { t } = useTranslation();
+  let router = useRouter();
 
-    const navLinksBorderAnimations = { borderBottom: '3px solid #fff' }
-    function handleClick() {
-        if (!pageMarginTop.isOpen) {
-            setPageMarginTop({
-                margin: '180px',
-                isOpen: true
-            })
-            return
-        }
-        setPageMarginTop({
-            margin: '100px',
-            isOpen: false
-        })
+  const navLinksBorderAnimations = { borderBottom: "3px solid #fff" };
+  function handleClick() {
+    if (!pageMarginTop.isOpen) {
+      setPageMarginTop({
+        margin: "180px",
+        isOpen: true,
+      });
+      return;
     }
+    setPageMarginTop({
+      margin: "100px",
+      isOpen: false,
+    });
+  }
 
-    return (
-        <header>
+  return (
+    <header>
+      <Navbar
+        expand="lg"
+        fixed="top"
+        className="navBarBG justify-content-around;"
+      >
+        <Link href="/" passHref className="LOGO">
+          <a className="link-logo">
+            <Image
+              className="logo-navbar"
+              src="/LOGO.png"
+              alt="LOGO"
+              width={150}
+              height={75}
+            />
+          </a>
+        </Link>
 
-            <Navbar expand="lg" fixed='top' className='navBarBG justify-content-around;'>
-
-                <Link href="/" passHref className="LOGO">
-                    <a className='link-logo'>
-                        <Image className='logo-navbar' src="/LOGO.png" alt="LOGO" width={150} height={75} />
-                    </a>
-                </Link>
-
-                <Navbar.Toggle onClick={(e) => handleClick()} aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav" className='justify-content-around'>
-                    <Row className='align-items-center'>
-                        <Col>
-                            <motion.span whileHover={navLinksBorderAnimations}>
-                                <NavLink href="/">
-                                    <a>HOME</a>
-                                </NavLink>
-                            </motion.span>
-                        </Col>
-                        <Col>
-                            <NavDropdown title="Raccontiamo Termini" id="nav-dropdown">
-                                <ul>
-                                    <motion.li whileHover={navLinksBorderAnimations}>
-                                        <NavLink href="/blog">
-                                            <a>BLOG</a>
-                                        </NavLink>
-                                    </motion.li>
-                                    <motion.li whileHover={navLinksBorderAnimations}>
-                                        <NavLink
-                                            href={{
-                                                pathname: "/blog",
-                                                query: {
-                                                    tag: "Progetti"
-                                                }
-                                            }}>
-                                            <a>PROGETTI</a>
-                                        </NavLink>
-                                    </motion.li>
-                                    <motion.li whileHover={navLinksBorderAnimations}>
-                                        <NavLink href={{
-                                            pathname: "/blog",
-                                            query: {
-                                                tag: "Eventi"
-                                            }
-                                        }}>
-                                            <a>Eventi</a>
-                                        </NavLink>
-                                    </motion.li>
-                                </ul>
-                            </NavDropdown>
-                        </Col>
-                        <Col>
-                            <motion.span whileHover={navLinksBorderAnimations}>
-                                <NavLink href="/chisiamo">
-                                    <a>MISSION</a>
-                                </NavLink>
-                            </motion.span>
-                        </Col>
-                        <Col>
-                            <motion.span whileHover={navLinksBorderAnimations}>
-                                <NavLink href="/contact">
-                                    <a>CONTATTACI</a>
-                                </NavLink>
-                            </motion.span>
-                        </Col>
-                        <Col>
-                            <NavDropdown title="Lingue" id="locales-dropdown">
-                                <ul>
-                                    {router.locales.map((locale) => {
-                                        return (
-                                            <li key={locale}>
-                                                <Link href={router.asPath} locale={locale}>
-                                                    <a>{locale === "it-IT" ? t('common:locale-it'):t('common:locale-uk')}</a>
-                                                </Link>
-                                            </li>
-                                        )
-                                    })}
-                                </ul>
-                            </NavDropdown>
-                        </Col>
-                    </Row>
-                </Navbar.Collapse>
-            </Navbar>
-        </header>
-    )
+        <Navbar.Toggle
+          onClick={(e) => handleClick()}
+          aria-controls="basic-navbar-nav"
+        />
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className="justify-content-around"
+        >
+          <Row className="align-items-center">
+            <Col>
+              <motion.span whileHover={navLinksBorderAnimations}>
+                <NavLink href="/">
+                  <a>{t("header:home")}</a>
+                </NavLink>
+              </motion.span>
+            </Col>
+            <Col>
+              <NavDropdown
+                title={t("header:raccontiamoTermini")}
+                id="nav-dropdown"
+              >
+                <ul>
+                  <motion.li whileHover={navLinksBorderAnimations}>
+                    <NavLink href="/blog">
+                      <a>{t("header:raccontiamoTermini1")}</a>
+                    </NavLink>
+                  </motion.li>
+                  <motion.li whileHover={navLinksBorderAnimations}>
+                    <NavLink
+                      href={{
+                        pathname: "/blog",
+                        query: {
+                          tag: "Progetti",
+                        },
+                      }}
+                    >
+                      <a>{t("header:raccontiamoTermini2")}</a>
+                    </NavLink>
+                  </motion.li>
+                  <motion.li whileHover={navLinksBorderAnimations}>
+                    <NavLink
+                      href={{
+                        pathname: "/blog",
+                        query: {
+                          tag: "Eventi",
+                        },
+                      }}
+                    >
+                      <a>{t("header:raccontiamoTermini3")}</a>
+                    </NavLink>
+                  </motion.li>
+                </ul>
+              </NavDropdown>
+            </Col>
+            <Col>
+              <motion.span whileHover={navLinksBorderAnimations}>
+                <NavLink href="/chisiamo">
+                  <a>{t("header:mission")}</a>
+                </NavLink>
+              </motion.span>
+            </Col>
+            <Col>
+              <motion.span whileHover={navLinksBorderAnimations}>
+                <NavLink href="/contact">
+                  <a>{t("header:contattaci")}</a>
+                </NavLink>
+              </motion.span>
+            </Col>
+            <Col>
+              <NavDropdown title="Lingue" id="locales-dropdown">
+                <ul>
+                  {router.locales.map((locale) => {
+                    return (
+                      <li key={locale}>
+                        <Link href={router.asPath} locale={locale}>
+                          <a>
+                            {locale === "it-IT"
+                              ? t("common:locale-it")
+                              : t("common:locale-uk")}
+                          </a>
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </NavDropdown>
+            </Col>
+          </Row>
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
+  );
 }
